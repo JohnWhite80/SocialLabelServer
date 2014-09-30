339ec0e57021937b55fb71661744fff9d4f02bec
John Bai <baichenhong@hotmail.com>
2014-09-30 11:05:12 +0800

    make some changes for ota

--- src/main/java/com/github/sociallabel/action/HomeController.java
+++ src/main/java/com/github/sociallabel/action/HomeController.java
@@ -3,7 +3,9 @@ package com.github.sociallabel.action;
 import java.io.File;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,20 +14,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.github.sociallabel.APIException;
 import com.github.sociallabel.service.UserService;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController extends AbsController {
+public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
@@ -68,5 +74,19 @@ public class HomeController extends AbsController {
         String headerValue = String.format("attachment; filename=\"%s\"", client.getName());
         response.setHeader(headerKey, headerValue);
 		return new FileSystemResource(client); 
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public @ResponseBody ResponseEntity<Map<String, String>> handleException(Exception e) {
+		logger.error("an exception occured", e);
+		HttpStatus errorCode = HttpStatus.INTERNAL_SERVER_ERROR;
+		String errorMessage = e.getMessage();
+		if(e instanceof APIException) {
+			errorCode = HttpStatus.valueOf(((APIException)e).getErrorCode());
+		}
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("code", String.valueOf(errorCode));
+		result.put("message", errorMessage);
+		return new ResponseEntity<Map<String, String>>(result, errorCode);
 	}
 }
